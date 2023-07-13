31f542be6f2bd76607ef63cf15f2e8c8f6df3f3c
Olaf Moriarty Solstrand <olafmoriarty@gmail.com>
2023-07-13 18:35:41 +0200

    Delete legacy code

--- budsjettapp-react/src/functions/prettyDate.ts
+++
@@ -1,7 +0,0 @@
-const prettyDate = (date : string) => {
-	const explodedDate = date.split('-');
-
-	return (explodedDate[2] + '.' + explodedDate[1] + '.' + explodedDate[0]);
-}
-
-export default prettyDate;
