fd9157f7a1af012c485c4da59c100aaa8ed9ca68
Olaf Moriarty Solstrand <olafmoriarty@gmail.com>
2023-08-26 22:48:41 +0200

    Very simple password verification for test environment added

--- budsjettapp-frontend/src/components/Content.tsx
+++ budsjettapp-frontend/src/components/Content.tsx
@@ -1,4 +1,4 @@
-import React, { lazy, Suspense } from 'react';
+import React, { useState, useEffect, lazy, Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Loading from './Loading';
 import StartPage from './pages/StartPage';
@@ -13,6 +13,41 @@ const BudgetScreen = lazy(() => import('./pages/BudgetScreen'));
 
 function Content() {
 	const bp = useBudget();
+
+	const [password, setPassword] = useState('');
+	const [loggedIn, setLoggedIn] = useState(true);
+	const [error, setError] = useState(false);
+
+	useEffect(() => {
+		const isLoggedIn = localStorage.getItem('isLoggedIn');
+		if (isLoggedIn !== 'YES') {
+			setLoggedIn(false);
+		}
+	}, []);
+
+	const checkPassword = (event : React.FormEvent) => {
+		event.preventDefault();
+		if (password === 'Hallgeir') {
+			localStorage.setItem('isLoggedIn', 'YES');
+			setLoggedIn(true);
+		}
+		else {
+			setError(true);
+		}
+	}
+
+	if (!loggedIn) {
+		return <main className="settings">
+			<form className="dialog-form budget-select-login" onSubmit={checkPassword}>
+			<h2>Oppgi invitasjonskode</h2>
+			{error ? <p className="error">Feil kode</p> : undefined}
+				<input type="password" value={password} onChange={(event) => setPassword(event.target.value)} />
+				<button className="button" type="submit">Få tilgang til Budsjett.app</button>
+			</form>
+			<p><a href="https://app.us9.list-manage.com/subscribe?u=7df3c4c3a46685b44f7d8682c&id=d09257588c">Send meg ein e-post når appen er tilgjengelig</a></p>
+		</main>
+	}
+
 	if (!bp.activeBudget) {
 		return <StartPage />
 	}
